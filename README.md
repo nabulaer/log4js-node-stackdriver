e17aa50ef4f41e356e68b7f935a2aca1afacdf42
Yosi Oren <yosi.oren@gmail.com>
2018-02-04 12:41:57 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # log4js-node-stackdriver
+an appender for the stackdriver on google cloud.
+
+to allow access to google cloud logging please follow setup and access permission on google cloud  documentation.
+You can use Application Default Credentials (ADC) or supply credential in config.
+
+By default all logging will be done to file syslog in the "Global" logging section.
+
+If you want you can specify a resource which the log will be written to.
+
+
+quick usage :
+```js
+var config ={
+    appenders: {
+        google: {
+            type:  'log4js-stackdriver-appender',
+            resource: {
+                type: "container",
+                labels: {
+                    cluster_name: 'local-dev',
+                    pod_id: process.env["HOSTNAME"],
+                    container_name: ''
+                }
+            },
+            logFileName: 'syslog',
+            layout: {
+                type: 'basic'
+            }
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['google'],
+            level:'DEBUG'
+        }
+    }
+};
+
+var log4js = require('log4js');
+log4js.configure(config);
+var logger = log4js.getLogger('local');
+logger.debug('somthing');
+```
---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ *  Created by yosi on 04/02/2018
+ */
+
+var Logging = require('@google-cloud/logging');
+
+
+function stackdriverAppender(logFileName, resource, layout, credentials) {
+
+    var logging = new Logging(credentials);
+    var log = logging.log(logFileName || "syslog");
+    return function (loggingEvent) {
+
+        var metadata = {
+            resource: {
+                type: "global",
+                labels: {
+                    HOSTNAME: process.env["HOSTNAME"]
+                }
+            },
+            severity: loggingEvent.level.levelStr === "WARN" ? "WARNING" : loggingEvent.level.levelStr
+
+        };
+        if (resource) {
+            metadata.resource = resource;
+        }
+        metadata.resource.labels.namespace_id = loggingEvent.categoryName;
+        var entry = log.entry(metadata, layout(loggingEvent));
+
+        log.write(entry);
+    };
+}
+
+function configure(config, layouts) {
+    var layout = layouts.basicLayout;
+    if (config.layout) {
+        layout = layouts.layout(config.layout.type, config.layout);
+    }
+
+    return stackdriverAppender(
+        config.logFileName,
+        config.resource,
+        layout,
+        config.credentials
+    );
+}
+
+module.exports.configure = configure;
